623d0c61d6f60be360f1bf3218ff508b153018cb
Bipin Suresh <bipsandbytes@yahoo.com>
2016-02-05 15:15:29 -0800

    Adapt to different DB adapters

--- lib/performance_promise/validations/full_table_scans.rb
+++ lib/performance_promise/validations/full_table_scans.rb
@@ -1,15 +1,44 @@
 module ValidateFullTableScans
   def validate_full_table_scans(db_queries, render_time, promised)
     full_table_scans = []
+    backtrace = []
 
     # check the explained queries to see if there were any
     # SCAN TABLEs
     db_queries.each do |db_query|
-      detail = db_query[:explained][0]['detail']
-      makes_full_table_scan = detail.match(/SCAN TABLE (.*)/)
-      if makes_full_table_scan
-        table_name = makes_full_table_scan[1]
-        full_table_scans << table_name
+      if db_query[:explained]
+        join_type = db_query[:explained].first[3]
+        if join_type
+
+          adapter_name = ActiveRecord::Base.connection.adapter_name
+          if adapter_name == 'Mysql2'
+            makes_full_table_scan = join_type.match(/ALL/)
+            table_name = db_query[:explained][0][2]
+          elsif adapter_name == 'SQLite'
+            makes_full_table_scan = join_type.match(/SCAN TABLE (.*)/)
+            table_name = makes_full_table_scan[1]
+          else
+            PerformancePromise.configuration.logger.warn("Unkown database adapter {adapter_name}")
+            makes_full_table_scan = join_type.match(/SCAN TABLE (.*)/)
+            table_name = makes_full_table_scan[1]
+          end
+
+          if makes_full_table_scan
+            full_table_scans << table_name
+
+            backtrace << db_query[:sql]
+            db_query[:trace].each do |trace|
+              if trace.starts_with?('app')
+                file, line_number = trace.split(':')
+                trace = '    |_' +
+                  File.read(file).split("\n")[line_number.to_i - 1].strip +
+                  ' (' + trace + ')'
+              end
+              backtrace << trace
+            end
+
+          end
+        end
       end
     end
 
@@ -22,7 +51,6 @@ module ValidateFullTableScans
     # check that the performed FTSs are a subset of the promised FTSs
     passes = (full_table_scans & promised_full_table_scans == full_table_scans)
     error_message = ''
-    backtrace = []
 
     unless passes
         error_message = "Promised table scans on #{promised_full_table_scans}, made: #{full_table_scans}"
